ddbd16a2224ab769a0d4cd2b33ea1eeade18c623
diogopaza <diogopazacvel@gmail.com>
2019-08-17 09:49:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/conversormoedas/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.conversormoedas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText valor = findViewById(R.id.edit_value);
+        valor.getText();
+
+        //ViewHolder
+        //ListView
+
+    }
+
+    private static class ViewHolder{
+
+    }
+}
